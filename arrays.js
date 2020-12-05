653fe5c9955b6cf9e70ffbcfd43a77ba09919746
eleung32 <52718704+eleung32@users.noreply.github.com>
2020-12-05 01:47:56 -0500

    es6 and traditional method

--- arrays.js
+++ arrays.js
@@ -1,17 +1,31 @@
 //reverse a string
 
 //ES6
-// const reverse = str => str.split('').reverse().join('');
+const reverse = str => str.split('').reverse().join('');
 
-// alert(reverse('What have you done?'));
+alert(reverse('What have you done?'));
 
 
 function reverse1(str) {
-    if ( str.length < 2 ){
-        return ('not a string');
-    }
+     if (  typeof str != 'string' ){
+         return ('not a string');
+     }
     return str.split('').reverse().join('');
 } 
 
-alert(reverse1(undefined));
+alert(reverse1('What have you done?'));
 
+
+function reverse2(str) {
+     if ( !str || str.length < 2 || typeof str != 'string'){
+         return ('not a string');
+     }
+     const reversedArray =[];
+     const totalItems = str.length-1;
+     for (let i= totalItems; i>=0; i--){
+         reversedArray.push(str[i]);
+     }
+     return reversedArray.join('');
+ }
+
+ alert(reverse2('What have you done?'));
